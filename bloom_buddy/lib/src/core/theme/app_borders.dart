1d58bf80363a5d7b933537b5a5ddf25281079327
Ghanyou Adam <ghanyouadam@gmail.com>
2025-09-13 20:02:29 +0000

    new config files

--- bloom_buddy/lib/src/core/theme/app_borders.dart
+++ bloom_buddy/lib/src/core/theme/app_borders.dart
@@ -6,6 +6,7 @@ class AppBorders {
   static const double radiusSmall = 8.0;
   static const double radiusMedium = 16;
   static const double radiusLarge = 24;
+
   static final BorderRadius circularSmall = BorderRadius.circular(radiusSmall);
   static final BorderRadius circularMedium = BorderRadius.circular(
     radiusMedium,
--- bloom_buddy/lib/src/core/theme/app_spacing.dart
+++ bloom_buddy/lib/src/core/theme/app_spacing.dart
@@ -6,4 +6,5 @@ class AppSpacing {
   static const double lg = 24.0;
   static const double xl = 32.0;
   static const double xxl = 40.0;
+  static const double xxxl = 48.0;
 }
